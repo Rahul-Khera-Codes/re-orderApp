3b796eef76a9ae7878ef30ca88efbbde3e763208
upexcel <upexcel>
2018-03-22 15:10:11 +0530

    update

--- server/app.js
+++ server/app.js
@@ -68,7 +68,7 @@ function withStoredProcedure(body, callback) {
   let product_line_data = [];
   con.query(`CALL sp_productcontrol(${body.email})`, function(err, list_Data) {
     product_list_data.push({ type: "table", name: "Product_Control_List", database: "reorderDB", data: list_Data })
-    console.log(list_data)
+    console.log(list_Data)
     findListData(list_Data[0], body.email, product_line_data, function(resposne) {
       product_list_data.push(response)
       callback(product_list_data)
