ff51336d597823b102533fdeac001f6089280ceb
upexcel <upexcel>
2018-03-23 14:13:44 +0530

    update

--- server/app.js
+++ server/app.js
@@ -645,6 +645,8 @@ app.post('/get/userData', function(req, res, next) {
           }
         }
       } else if (barCode) {
+        let login_data = [];
+        let body = {};
         login_data.push({ type: 'table', name: 'Customer_Table', database: 'reorderDB', data: customer_data })
         login_data.push({ type: 'table', name: 'Contact_Table', database: 'reorderDB', data: contact_data })
         body['data'] = login_data;
