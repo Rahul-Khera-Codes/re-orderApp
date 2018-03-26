e55bb5cd0a8a0332bc3d613dd90cfa3d50aba4b2
upexcel <upexcel>
2018-03-26 13:24:35 +0530

    update for product codes

--- server/app.js
+++ server/app.js
@@ -79,8 +79,8 @@ function findListData(list_data, email, line_data, callback) {
           product.push(Product);
           if (line_data.length == key + 1) {
             let product_id = _.map(product, 'ID');
-            // console.log(product_id)
-            con.query(`select * from productcodes where ProductIDLocal IN ${product_id}`, function(err, product_codes) {
+            console.log(product_id)
+            con.query(`select * from productcodes where ProductIDLocal IN ${[product_id]}`, function(err, product_codes) {
               console.log(err, product_codes)
               let final_data = [{ type: "table", name: "Product", database: "reorderDB", data: product }, { type: "table", name: "Product_Control_Line", database: "reorderDB", data: product_line }, { type: "table", name: "ProductCodes", database: "reorderDB", data: product_codes }]
               callback(final_data)
@@ -89,8 +89,8 @@ function findListData(list_data, email, line_data, callback) {
         })
       } else {
         let product_id = _.map(product, 'ID');
-        // console.log(product_id)
-        con.query(`select * from productcodes where ProductIDLocal IN ${product_id}`, function(err, product_codes) {
+        console.log(product_id)
+        con.query(`select * from productcodes where ProductIDLocal IN ${[product_id]}`, function(err, product_codes) {
           console.log(err, product_codes)
           let final_data = [{ type: "table", name: "Product", database: "reorderDB", data: product }, { type: "table", name: "Product_Control_Line", database: "reorderDB", data: product_line }, { type: "table", name: "ProductCodes", database: "reorderDB", data: product_codes }]
           callback(final_data)
